f778c9f581c16b465a9b937e0d151b7660675ed3
baeksong.github <baeksong.github@gmail.com>
2023-03-23 19:16:28 +0900

    service 추가 및 빈 등록

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,20 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    private  HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+
+    //@GetMapping("/hello")
     public String hello(String name){
-        return "Hello" + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -22,28 +23,28 @@ import java.io.IOException;
 @SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
 
-		//SpringApplication.run(HellobootApplication.class, args);
+	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.refresh();
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			 servletContext.addServlet("frontcontroller", new HttpServlet() {
 				 @Override
 				 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					 //인증 , 보안, 다국어, 공통 기능
-					 HelloController helloController = new HelloController();
-
 					 if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						 String name = req.getParameter("name");
 
-					 String name = req.getParameter("name");
-					 String hello = helloController.hello(name);
+						 HelloController helloController = applicationContext.getBean(HelloController.class);
 
-					 resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().print(hello);
-					 } else if (req.getRequestURI().equals("/user")) {
-
-					 }else{
+						 String hello = helloController.hello(name);
+						 resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						 resp.getWriter().print(hello);
+					 } else{
 						 resp.setStatus(HttpStatus.NOT_FOUND.value());
 					 }
 				 }
